846b7f71ac3d3d44ada72a781d0f4709e0398292
Siyi Ji <sylviajsyy@gmail.com>
2026-04-08 20:40:43 -0500

    Add setuptests.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -12,11 +13,10 @@ export default ({ mode }) => {
         '/api': env.VITE_API_URL,
       },
     },
+    test: {
+      environment: "jsdom",
+      globals: true,
+      setupFiles: "./src/setupTests.js",
+    },
   });
 };
-
-// test: {
-  //   environment: "jsdom",
-  //   globals: true,
-  //   setupFiles: "./src/setupTests.js",
-  // },
